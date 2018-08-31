386d6db9dbbb9df71da0a0d5c116640b23d4b864
Patrick Hobusch <patrick.hobusch@gmail.com>
2018-08-31 09:49:21 +0200

    Initial commit

---
+++ include/component1/component1.h
@@ -0,0 +1,11 @@
+#ifndef __COMPONENT1_H__
+#define __COMPONENT1_H__
+
+class component1 {
+
+public:
+    void print();
+
+};
+
+#endif
---
+++ src/component1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "component1/component1.h"
+
+void component1::print() {
+    std::cout << "component1" << std::endl;
+}
